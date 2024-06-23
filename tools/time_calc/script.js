3509f2e7aa1e648fef8294773e30081fa6812eff
moyou <yangshilong1997@gmail.com>
2024-06-23 16:15:57 -0400

    finalized on the file conversion from nd2info to real time

--- tools/time_calc/script.js
+++ tools/time_calc/script.js
@@ -3,17 +3,26 @@ function convertJulianToGregorian() {
     const timezone = document.getElementById('timezone').value;
 
     if (julianDay) {
-        const millisecondsPerDay = 86400000; // 24 * 60 * 60 * 1000
+        /*const millisecondsPerDay = 86400000; // 24 * 60 * 60 * 1000
         const gregorianEpochMilliseconds = -210866760000000; // Milliseconds from 0000-01-01 to 1970-01-01
       
         const milliseconds = Math.floor(julianDay * millisecondsPerDay + gregorianEpochMilliseconds);
-        const date = new Date(milliseconds);
-        const formattedDate = convertToTimeZone(date, timezone);
+        const date = new Date(milliseconds); */
+        const formattedDate = convertToTimeZone(J2Gconversion(julianDay), timezone);
 
         document.getElementById('gregorianDate').innerText = `Gregorian Date: ${formattedDate}`;
     } else {
         document.getElementById('gregorianDate').innerText = 'Please enter a valid Julian day.';
     }
+}
+
+function J2Gconversion(julianDay) {
+    const millisecondsPerDay = 86400000; // 24 * 60 * 60 * 1000
+    const gregorianEpochMilliseconds = -210866760000000; // Milliseconds from 0000-01-01 to 1970-01-01
+    
+    const milliseconds = Math.floor(julianDay * millisecondsPerDay + gregorianEpochMilliseconds);
+    const date = new Date(milliseconds);
+    return date;
 }
 
 function convertToTimeZone(date, timeZone) {
@@ -25,15 +34,22 @@ function convertToTimeZone(date, timeZone) {
 function convertGregorianToJulian() {
     const gregorianDate = document.getElementById('gregorianDateInput').value;
     const timezone = document.getElementById('timezone').value;
-    const { DateTime } = luxon;
+    /*const { DateTime } = luxon;
     const dt = DateTime.fromFormat(gregorianDate, 'yyyy-MM-dd HH:mm:ss.SSS', { zone: timezone });
-
-    if (dt) {
-        const julianDay = dt.toMillis() / 86400000 + 2440587.5;
-        document.getElementById('julianDayOutput').innerText = `Julian Day: ${julianDay}`;
+*/
+    if (gregorianDate) {
+        //const julianDay = dt.toMillis() / 86400000 + 2440587.5;
+        document.getElementById('julianDayOutput').innerText = `Julian Day: ${G2Jconversion(gregorianDate,timezone)}`;
     } else {
         document.getElementById('julianDayOutput').innerText = 'Please enter a valid Gregorian date.';
     }
+}
+
+function G2Jconversion(gregorianDate,timezone) {
+    const { DateTime } = luxon;
+    const dt = DateTime.fromFormat(gregorianDate, 'yyyy-MM-dd HH:mm:ss.SSS', { zone: timezone });
+    const julianDay = dt.toMillis() / 86400000 + 2440587.5;
+    return julianDay;
 }
 
 function showCurrentJulian() {
@@ -44,6 +60,7 @@ function showCurrentJulian() {
 
 function convertnd2() {
     const fileInput = document.getElementById('fileInput');
+    const timeOffset = document.getElementById('timeOffset').value;
     const timezone = document.getElementById('timezone').value;
     
 
@@ -59,16 +76,17 @@ function convertnd2() {
         const lines = event.target.result.split('\n');
         //let resultHtml = '<h2>Search Results:</h2>';
         let searchResult = '';
-        const regex_timeab = /(dTimeAbsolute)\s*=\s*(\d+)/;
+        const regex_timeab = /(dTimeAbsolute)\s*=\s*([\d.]+)/;
         const regex_stamp = /(timestamp)\s*#(\d+)\s*=\s*([\d.]+)/;
         let countframe = 0;
         const framenum = [];
         const frametime = [];
+        let abtime = 0;
         lines.forEach((line, index) => {
             const match1 = line.match(regex_timeab);
             if (match1) {
                 //resultHtml += `<p>Line ${index + 1}: ${line}</p>`;
-                const abtime = match1[2];
+                abtime = match1[2];
             }
             const match2 = line.match(regex_stamp);
             if (match2) {
@@ -80,17 +98,40 @@ function convertnd2() {
 
         
         if (framenum.length) {
-            let resultcsv = 'Frame,Time\n';
-            for (let i = 0; i < framenum.length; i++) {
-                resultcsv += `${framenum[i]},${frametime[i]}\n`;
+            if (timeOffset) {
+                let resultcsv = 'Frame,Time\n';
+                const fileDate = J2Gconversion(abtime);
+                const formattedFileDate = convertToTimeZone(fileDate,timezone);
+                const { DateTime } = luxon;
+                dtFile = DateTime.fromFormat(formattedFileDate, 'yyyy-MM-dd HH:mm:ss.SSS', { zone: timezone });
+                dtOffset = DateTime.fromFormat(timeOffset, 'yyyy-MM-dd HH:mm:ss.SSS', { zone: timezone });
+                const diff = dtFile.diff(dtOffset).milliseconds;
+                for (let i = 0; i < framenum.length; i++) {
+                    let difftime = parseFloat(frametime[i]) + diff/1000;
+                    resultcsv += `${framenum[i]},${difftime.toString()}\n`;
+                }
+                const blob = new Blob([resultcsv], { type: 'text/csv' });
+                const url = URL.createObjectURL(blob);
+                const downloadLink = document.getElementById('nd2link');
+                downloadLink.href = url;
+                downloadLink.download = file.name.replace(/\.[^/.]+$/, "") + '_offset.csv';
+                downloadLink.style.display = 'block';
+                downloadLink.textContent = 'Download Offset Results';
+
+            } else {
+                let resultcsv = 'Frame,Time\n';
+                for (let i = 0; i < framenum.length; i++) {
+                    resultcsv += `${framenum[i]},${frametime[i]}\n`;
+                }
+                const blob = new Blob([resultcsv], { type: 'text/csv' });
+                const url = URL.createObjectURL(blob);
+                const downloadLink = document.getElementById('nd2link');
+                downloadLink.href = url;
+                downloadLink.download = file.name.replace(/\.[^/.]+$/, "") + '_raw.csv';
+                downloadLink.style.display = 'block';
+                downloadLink.textContent = 'Download Raw Results';
             }
-            const blob = new Blob([resultcsv], { type: 'text/csv' });
-            const url = URL.createObjectURL(blob);
-            const downloadLink = document.getElementById('nd2link');
-            downloadLink.href = url;
-            downloadLink.download = 'search_result.txt';
-            downloadLink.style.display = 'block';
-            downloadLink.textContent = 'Download Results';
+            
         }
     };
 
